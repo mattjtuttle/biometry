627628d164e7988caa595b68003173b5f59195a8
Matt Tuttle <mtuttle4@vols.utk.edu>
2017-08-24 12:05:58 -0400

    initial commit

---
+++ Weekly_assignments/week01/Homework 1.R
@@ -0,0 +1,63 @@
+#Return to eebbiometry@gmail.com by 1:25pm, Monday August 28.
+#   on the subject line put    YOURLASTNAME_EXERCISE
+
+#Name:
+
+
+#1. Assign to a vector called X the values 1,3,5,6,7, and 9. Assign to a vector called Y the values 2,7,4,5,2 and 12.
+
+
+
+ 
+#2. Combine the two vectors together as one large vector called Z.
+
+
+
+#3. Create a new vector that includes all the values in Z that are less than 6.
+
+
+
+#4. Using R functions, calculate the mean of Z.
+
+
+
+#5. Create an array where the columns are X and Y. Assign this array to an object called Q.
+
+
+
+#6. Using R functions, calculate the sum of each row.
+
+
+
+#7. Calculate the mean of the rows of Q and add this as a third column.
+
+
+
+#8. Extract the value of Q at the second row and first column.
+
+
+
+#9. Write Q to a .csv file.
+
+
+
+#10. Read the .csv file and assign it to an object.
+
+
+
+
+#11 Show that the third number in the first column of Q is equal to the fourth number in the second column.
+
+
+
+#12 Make 'Q' a data frame.
+
+
+
+#13 Using R functions, calculate the length of X.
+
+
+
+#14 Assign the name "mean" to the third column of Q.
+
+
