6beccab0b350017cc3cc0fc91338aa43f6eddb76
Matt Tuttle <mtuttle4@vols.utk.edu>
2017-09-20 09:39:20 -0400

    Downloaded weekly assignment 4

---
+++ Weekly_assignments/week04_in_class/15 September inclass.R
@@ -0,0 +1,22 @@
+#Inclass and homework for the week.
+
+
+
+# In an effort to keep up with rising starfruit demand, you begin taking measurements of yields of fruit from individual plants. Below is data of fruit mass in grams from each plant.
+fruit.mass.g<-c(9.98,37.87,16.72,9.83,6.01,30.08,59.23,8.15,246.81,8.2,0.12,3.52,218.36,73.75,5.99,2.63,155.61,3.85,90.17,27.18,8.88,6.5,1.39,234.51,1.07,22.94,3,0.82,0.6,551.9,14.59,21.63,50.24,126.92,5.3,35.75,2.11,411.13,142.95,10.27)
+
+
+#1. Use a histogram to look at the shape of your fruit mass data.
+
+
+#2. Log transform the data and use a histogram to look at the shape of your fruit mass data.
+
+
+#3. Calculate the 95% CI and provide the answer in grams. (Hint: think about how your data is transformed!) 
+
+
+
+#4 Wow! You found a super tree. It yields 754 g of starfruit. How exceptional is this tree? (What is the probability of finding that yield or greater based on the data?)
+
+
+
